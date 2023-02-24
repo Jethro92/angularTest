1d8db437b0ec331b887de643671132bfe07f41f9
Geoffry <jeffryhu@hotmail.com>
2023-02-24 17:24:05 -0600

    fixing server config to receive request from the client

--- express-server/app.js
+++ express-server/app.js
@@ -1,6 +1,8 @@
 let express = require("express");
+let cors = require('cors')
 let  app = express();
 app.use(express.json());
+app.use(cors());
 app.listen(3000, () => console.log("Server running on port 3000"));
 
 app.get("/url", (req,res,next) => res.json(["Paris", "Barcelona", "Barranquilla", "Montevideo", "Santiago de Chile","Mexico DF", "New York"]));
@@ -9,8 +11,10 @@ let misDestinos = [];
 app.get("/my", (req,res,next) => res.json(misDestinos));
 
 app.post("/my", (req,res,next) => {
-    console.log(req.body);
-    misDestinos = req.body;
 
-    res.status(200).send("hi: "+ misDestinos);
+    console.log(req.body);
+    //misDestinos = req.body;
+    misDestinos.push(req.body)
+
+    res.status(200).send(req.body);
 });
